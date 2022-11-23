cacbf461c5a6c0f7b14f44dc9c1d144846a0afd1
Christos_Krilis <christos.krilis.dev@gmail.com>
2022-11-23 02:53:17 +0200

    Update TrainingStateWindow.cs

--- Project/Assets/ML-Agents/PF/Scripts/RL/Editor/TrainingStateWindow.cs
+++ Project/Assets/ML-Agents/PF/Scripts/RL/Editor/TrainingStateWindow.cs
@@ -1,60 +1,65 @@
-﻿using UnityEditor;
+﻿using ML_Agents.PF.Scripts.Enums;
+using UnityEditor;
 using UnityEngine;
-
 
 namespace ML_Agents.PF.Scripts.RL.Editor
 {
     public class TrainingStateWindow : EditorWindow
     {
-        /// <summary>
-        /// condition list
-        ///
-        /// </summary>
-        private int _prefixLabelWidth = 120;
-        private int _inputFieldWidth = 160;
 
-        private static bool _trainingFoldoutValue = true;
-        private static bool _nodesFoldoutValue = false;
-        // private static bool _graphFoldoutValue = false;
+        [MenuItem("RL/Training Info Window", priority = 0)]
+        public static void ShowWindow()
+        {
+            GetWindow<TrainingStateWindow>("Current Training Info");
+            _liveUpdate = EditorPrefs.GetBool(TRAINING_WINDOW_LIVE_UPDATE);
+        }
 
-        private static bool _liveUpdate;
-        private static string _trainignWindowLiveUpdate = "TrainingWindow_LiveUpdate";
-        private Vector2 _scrollPosition;
-        private GUIStyle _moraleStyle;
-        private int _tempInt;
+    #region Vars
 
         private PathFindAgent _agent;
         private PathFindAgent.AgentExpose _agentExpose;
+
+        private static bool _trainingFoldoutValue = true;
+        private static bool _nodesFoldoutValue = true;
+        // private static bool _graphFoldoutValue = true;
+        private static bool _conditionsFoldoutValue = true;
+        private static bool _rewardFoldoutValue = true;
+
+        private const string TRAINING_WINDOW_LIVE_UPDATE = "TrainingWindow_LiveUpdate_Key";
+        private static bool _liveUpdate = true;
+
+        private Vector2 _scrollPosition;
+        private GUIStyle _moraleStyle;
+        private int _tempInt;
+        private int _prefixLabelWidth = 120;
+        private int _inputFieldWidth = 160;
+
+    #endregion
+
+
+    #region Control
 
         private static bool LiveUpdate
         {
             get => _liveUpdate;
             set
             {
-                EditorPrefs.SetBool("CharacterExposeWindow_LiveUpdate", value);
+                EditorPrefs.SetBool(TRAINING_WINDOW_LIVE_UPDATE, value);
                 _liveUpdate = value;
             }
         }
-
-        [MenuItem("RL/Training Info Window", priority = 0)]
-        public static void ShowWindow()
-        {
-            GetWindow<TrainingStateWindow>("Current Training Info");
-            _liveUpdate = EditorPrefs.GetBool(_trainignWindowLiveUpdate);
-        }
-
         protected void OnEnable()
         {
             EditorApplication.playModeStateChanged += PauseStateChanged;
-            ClearCharacter();
-            _liveUpdate = EditorPrefs.GetBool(_trainignWindowLiveUpdate);
+            ClearAgent();
+            _liveUpdate = EditorPrefs.GetBool(TRAINING_WINDOW_LIVE_UPDATE);
         }
 
         private void OnDisable()
         {
             EditorApplication.playModeStateChanged -= PauseStateChanged;
-            ClearCharacter();
-            _liveUpdate = EditorPrefs.GetBool(_trainignWindowLiveUpdate);
+            ClearAgent();
+            _liveUpdate = EditorPrefs.GetBool(TRAINING_WINDOW_LIVE_UPDATE);
         }
 
         private void PauseStateChanged(PlayModeStateChange state)
@@ -62,27 +67,23 @@ namespace ML_Agents.PF.Scripts.RL.Editor
             switch (state)
             {
                 case PlayModeStateChange.EnteredPlayMode:
-                    _liveUpdate = EditorPrefs.GetBool(_trainignWindowLiveUpdate);
+                    _liveUpdate = EditorPrefs.GetBool(TRAINING_WINDOW_LIVE_UPDATE);
 
                     break;
 
                 case PlayModeStateChange.EnteredEditMode:
-                    ClearCharacter();
+                    ClearAgent();
 
                     break;
             }
         }
 
-        private void ClearCharacter()
+        private void ClearAgent()
         {
-            //ItemsFoldoutValues.Clear();
-            //CardsFoldoutValues.Clear();
-            //_exposedCharacter = null;
-            //_aiSelectedMoves.Clear();
             Repaint();
         }
 
-        private void TryGetCharacter()
+        private void TryGetAgent()
         {
             _agent = FindObjectOfType<PathFindAgent>();
             _agentExpose = new PathFindAgent.AgentExpose(_agent);
@@ -95,7 +96,7 @@ namespace ML_Agents.PF.Scripts.RL.Editor
 
             if (_agent == null)
             {
-                TryGetCharacter();
+                TryGetAgent();
             }
             else
             {
@@ -103,12 +104,16 @@ namespace ML_Agents.PF.Scripts.RL.Editor
             }
         }
 
+    #endregion
+
+    #region GUI Forms
+
         protected void OnGUI()
         {
             _inputFieldWidth = (int)position.width / 3;
             _prefixLabelWidth = _inputFieldWidth - 40;
 
-            if(!Application.isPlaying) return;
+            if (!Application.isPlaying) return;
 
             if (_agent == null)
             {
@@ -116,7 +121,7 @@ namespace ML_Agents.PF.Scripts.RL.Editor
 
                 if (GUILayout.Button("Get Agent"))
                 {
-                    TryGetCharacter();
+                    TryGetAgent();
 
                     return;
                 }
@@ -137,22 +142,9 @@ namespace ML_Agents.PF.Scripts.RL.Editor
             EditorGUILayout.Space();
             NodesInfo();
             EditorGUILayout.Space();
-            // Morality();
+            ConditionsData();
             EditorGUILayout.Space();
-
-            //Passage();
-            EditorGUILayout.Space();
-            CreateIndentedLabel("ConditionsData : ", _agentExpose.ConditionsData.ToString());
-            CreateIndentedLabel("RewardDataStruct : ", _agentExpose.RewardDataStruct.ToString());
-            CreateIndentedLabel("RewardConditions : ", _agentExpose.RewardConditions.ToString());
-            CreateIndentedLabel("RewardDataStructFromT : ", _agentExpose.RewardDataStructFromT.ToString());
-
-            //Deck();
-            EditorGUILayout.Space();
-            //Inventory();
-            EditorGUILayout.Space();
-            //AIMoves();
-            EditorGUILayout.Space();
+            // RewardInfo();
 
             EditorGUIUtility.labelWidth = labelWidth;
         }
@@ -166,14 +158,13 @@ namespace ML_Agents.PF.Scripts.RL.Editor
 
             EditorGUILayout.ObjectField(_agent, typeof(GameObject), false);
             EditorGUILayout.ObjectField(_agentExpose.Area, typeof(GameObject), false);
-            EditorGUILayout.ObjectField(_agentExpose.Graph, typeof(GameObject), false);
         }
 
         private void TrainingInfo()
         {
             BeginVerticalBoxArea();
 
-            _trainingFoldoutValue = EditorGUILayout.Foldout(_trainingFoldoutValue, "-Training-");
+            _trainingFoldoutValue = EditorGUILayout.Foldout(_trainingFoldoutValue, "-Training Info-");
 
             if (_trainingFoldoutValue)
             {
@@ -193,7 +184,7 @@ namespace ML_Agents.PF.Scripts.RL.Editor
         {
             BeginVerticalBoxArea();
 
-            _nodesFoldoutValue = EditorGUILayout.Foldout(_nodesFoldoutValue, "Nodes Info");
+            _nodesFoldoutValue = EditorGUILayout.Foldout(_nodesFoldoutValue, "-Nodes Info-");
 
             if (_nodesFoldoutValue)
             {
@@ -201,13 +192,78 @@ namespace ML_Agents.PF.Scripts.RL.Editor
                 EditorGUI.indentLevel++;
 
                 CreateIndentedLabel("Target Index : ", _agentExpose.TargetIndex.ToString());
-                EditorGUILayout.ObjectField(_agentExpose.CurrentTarget, typeof(GameObject), false);
+                CreateIndentedLabel("Target Name : ", _agentExpose.CurrentTarget.ToString());
+                EditorGUILayout.Space();
 
                 foreach (var node in _agentExpose.NodesToFind)
                 {
                     EditorGUILayout.ObjectField(node, typeof(GameObject), false);
                 }
+                EditorGUI.indentLevel = index;
+            }
+            EndVerticalBoxArea();
+        }
 
+        private void ConditionsData()
+        {
+            BeginVerticalBoxArea();
+
+            _conditionsFoldoutValue = EditorGUILayout.Foldout(_conditionsFoldoutValue, "-Conditions Info-");
+            var conditionsData = _agentExpose.ConditionsData;
+
+            if (_conditionsFoldoutValue)
+            {
+                var index = EditorGUI.indentLevel;
+                EditorGUI.indentLevel++;
+
+                EditorGUILayout.ObjectField(_agentExpose.Graph, typeof(GameObject), false);
+
+                EditorGUILayout.Space();
+                CreateIndentedLabel("Max Step : ", conditionsData.MaxStep.ToString());
+                CreateIndentedLabel($"Step Count ", $"{conditionsData.StepCount.ToString()}/{conditionsData.MaxStep.ToString()}");
+                CreateIndentedLabel("Step Factor : ", conditionsData.StepFactor.ToString("f2"));
+
+                EditorGUILayout.Space();
+                CreateIndentedLabel("Path Length : ", conditionsData.FullPathLength.ToString());
+                CreateIndentedLabel("CP Length : ", conditionsData.CheckPointPathLength.ToString());
+
+                var tmp = (int)_agentExpose.PhaseType >= 3 ? conditionsData.FullPathLength.ToString() : conditionsData.StepCount.ToString();
+                CreateIndentedLabel($"Traveled Distance :", "{conditionsData.TraveledDistance.ToString()}/{tmp}");
+
+                EditorGUILayout.Space();
+                CreateIndentedLabel("Found Check point : ", conditionsData.HasFoundCheckpoint.ToString());
+                CreateIndentedLabel("Found Goal : ", conditionsData.HasFoundGoal.ToString());
+                CreateIndentedLabel("Touched Wall: ", conditionsData.HasTouchedWall.ToString());
+
+                EditorGUI.indentLevel = index;
+            }
+            EndVerticalBoxArea();
+        }
+
+//graph, path, nodes path
+
+
+        private void RewardInfo()
+        {
+            BeginVerticalBoxArea();
+
+            _rewardFoldoutValue = EditorGUILayout.Foldout(_rewardFoldoutValue, "-Reward Data Info-");
+            var rewardCondition = _agentExpose.RewardDataStruct.Conditions;
+
+            if (_rewardFoldoutValue)
+            {
+                var index = EditorGUI.indentLevel;
+                EditorGUI.indentLevel++;
+
+
+                EditorGUILayout.Space();
+
+                CreateIndentedLabel("Conditions Sizes : ", rewardCondition.Length.ToString());
+
+                for (int i = 0; i < rewardCondition.Length; i++)
+                {
+                    CreateIndentedLabel($"Conditions {i + 1}: ", rewardCondition[i].ToString());
+                }
 
                 EditorGUI.indentLevel = index;
             }
@@ -215,44 +271,10 @@ namespace ML_Agents.PF.Scripts.RL.Editor
         }
 
 
-        // private void Morality()
-        // {
-        //     BeginVerticalBoxArea();
-        //
-        //     _graphFoldoutValue = EditorGUILayout.Foldout(_graphFoldoutValue, "Graph Info");
-        //
-        //     if (_graphFoldoutValue)
-        //     {
-        //         var index = EditorGUI.indentLevel;
-        //         EditorGUI.indentLevel++;
-        //
-        //         EditorGUILayout.BeginHorizontal(_moraleStyle);
-        //         CreateMoraleColumn(_agentExpose.Graph.Nodes, typeof(Sin), "Sins");
-        //         CreateMoraleColumn(_exposedCharacter.Virtues, typeof(Virtue), "Virtues");
-        //         EditorGUILayout.EndHorizontal();
-        //
-        //         EditorGUI.indentLevel = index;
-        //     }
-        //     EndVerticalBoxArea();
-        // }
+    #endregion
 
 
-        // private void CreateMoraleColumn(int[] moraleValues, Type moraleType, string moraleName)
-        // {
-        //     EditorGUILayout.BeginVertical();
-        //     EditorGUILayout.LabelField(moraleName, EditorStyles.boldLabel);
-        //
-        //     for (var index = 0; index < moraleValues.Length; index++)
-        //     {
-        //         if (CreateIntField(Enum.GetName(moraleType, index), moraleValues[index], out _tempInt))
-        //         {
-        //             if (moraleType == typeof(Sin)) _exposedCharacter.SetSin((Sin) index, _tempInt);
-        //             else if (moraleType == typeof(Virtue)) _exposedCharacter.SetVirtue((Virtue) index, _tempInt);
-        //         }
-        //     }
-        //     EditorGUILayout.EndVertical();
-        // }
-
+    #region GUI Methods
 
         private void CreateIndentedLabel(string label, string value)
         {
@@ -260,34 +282,6 @@ namespace ML_Agents.PF.Scripts.RL.Editor
             EditorGUILayout.TextField(label, value, EditorStyles.boldLabel, GUILayout.Width(_inputFieldWidth + 48));
             EditorGUILayout.EndHorizontal();
         }
-
-        private bool CreateIntField(string label, int value, out int newValue)
-        {
-            EditorGUILayout.BeginHorizontal();
-
-            EditorGUI.BeginChangeCheck();
-            newValue = EditorGUILayout.IntField(label, value, GUILayout.Width(_inputFieldWidth));
-            bool hasChanged = EditorGUI.EndChangeCheck();
-
-            GUILayout.Space(4);
-
-            if (GUILayout.Button("-", GUILayout.Width(20)))
-            {
-                newValue -= 1;
-                hasChanged = true;
-            }
-
-            if (GUILayout.Button("+", GUILayout.Width(20)))
-            {
-                newValue += 1;
-                hasChanged = true;
-            }
-
-            EditorGUILayout.EndHorizontal();
-
-            return hasChanged;
-        }
-
 
         private void BeginVerticalBoxArea()
         {
@@ -302,31 +296,7 @@ namespace ML_Agents.PF.Scripts.RL.Editor
             EditorGUILayout.Space();
         }
 
-        private void DisplayIcon(Sprite sprite, float multiplier = 1, float xDistance = 0)
-        {
-            var myRectPos = GUILayoutUtility.GetLastRect();
-            myRectPos.x += 25 + myRectPos.width + xDistance;
-            myRectPos.width = multiplier * 50;
-            myRectPos.height = myRectPos.width * ((float)sprite.texture.height / sprite.texture.width);
-            UnityEngine.GUI.DrawTexture(myRectPos, sprite.texture, ScaleMode.StretchToFill);
-            EditorGUILayout.Space(myRectPos.height);
-        }
-
-        // private void CreateMoraleColumn(int[] moraleValues, Type moraleType, string moraleName)
-        // {
-        //     EditorGUILayout.BeginVertical();
-        //     EditorGUILayout.LabelField(moraleName, EditorStyles.boldLabel);
-        //
-        //     for (var index = 0; index < moraleValues.Length; index++)
-        //     {
-        //         if (CreateIntField(Enum.GetName(moraleType, index), moraleValues[index], out _tempInt))
-        //         {
-        //             if (moraleType == typeof(Sin)) _exposedCharacter.SetSin((Sin) index, _tempInt);
-        //             else if (moraleType == typeof(Virtue)) _exposedCharacter.SetVirtue((Virtue) index, _tempInt);
-        //         }
-        //     }
-        //     EditorGUILayout.EndVertical();
-        // }
+    #endregion
 
     }
 }
