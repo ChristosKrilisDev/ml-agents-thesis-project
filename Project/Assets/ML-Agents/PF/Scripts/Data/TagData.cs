a2a4fa0a9b7aa8bceea3f21dbbe8114c81453d73
Christos_Krilis <christos.krilis.dev@gmail.com>
2023-01-31 21:24:00 +0200

    fixed minor tag issue

--- Project/Assets/ML-Agents/PF/Scripts/Data/TagData.cs
+++ Project/Assets/ML-Agents/PF/Scripts/Data/TagData.cs
@@ -6,7 +6,7 @@
         public const string OBJECT_TAG = "pfobj";
         public const string SPAWN_AREA_TAG = "spawnArea";
 
-        public const string WALL_TAG = "reward";
+        public const string WALL_TAG = "wall";
         public const string GOAL_TAG = "spawnArea";
         public const string AGENT_TAG = "agent";
 
